46111f268aaaa0eb8c5e21234709041db05a01e8
alessiavozzo <a.vozzo95@gmail.com>
2024-05-09 16:11:06 +0200

    add footer basic style

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,28 @@
-<footer>
-    sono un footer
+<footer class="bg-dark text-white py-3">
+    <div class="container">
+        <div class="row row-cols-3">
+            <ul class="list-unstyled">
+                <li><a href="#" class="text-decoration-none text-white">Lorem, ipsum.</a></li>
+                <li><a href="#" class="text-decoration-none text-white">Velit, placeat.</a></li>
+                <li><a href="#" class="text-decoration-none text-white">Optio, voluptates.</a></li>
+                <li><a href="#" class="text-decoration-none text-white">Odit, sit.</a></li>
+            </ul>
+            <ul class="list-unstyled">
+                <li><a href="#" class="text-decoration-none text-white">Lorem, ipsum.</a></li>
+                <li><a href="#" class="text-decoration-none text-white">Velit, placeat.</a></li>
+                <li><a href="#" class="text-decoration-none text-white">Optio, voluptates.</a></li>
+                <li><a href="#" class="text-decoration-none text-white">Odit, sit.</a></li>
+            </ul>
+            <ul class="list-unstyled">
+                <li><a href="#" class="text-decoration-none text-white">Lorem, ipsum.</a></li>
+                <li><a href="#" class="text-decoration-none text-white">Velit, placeat.</a></li>
+                <li><a href="#" class="text-decoration-none text-white">Optio, voluptates.</a></li>
+                <li><a href="#" class="text-decoration-none text-white">Odit, sit.</a></li>
+            </ul>
+
+        </div>
+
+    </div>
+
+
 </footer>
