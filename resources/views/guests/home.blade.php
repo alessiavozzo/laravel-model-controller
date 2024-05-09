a8d98cfbb68aed9c7fe2337b0894288e8fac9073
alessiavozzo <a.vozzo95@gmail.com>
2024-05-09 16:03:44 +0200

    add awful cards style

--- resources/views/guests/home.blade.php
+++ resources/views/guests/home.blade.php
@@ -4,25 +4,27 @@
     {{-- movies container --}}
     <section id="movies">
         <div class="container">
-            <div class="row row-cols-1 row-cols-md-2">
+            <div class="row row-cols-1 row-cols-md-2 gy-3">
 
                 @forelse ($movies as $movie)
                     <div class="col">
-                        <div class="card">
+                        <div class="movie card bg-dark">
                             <div class="card-body">
                                 {{-- title --}}
-                                <h4 class="card-title">{{ $movie->title }}</h4>
+                                <h4 class="card-title text-center border-bottom">{{ $movie->title }}</h4>
                                 {{-- original title --}}
-                                <div class="original-title"><strong>Titolo originale:</strong>{{ $movie->original_title }}
+                                <div class="original-title mb-2 text-white"><strong>Titolo
+                                        originale: </strong>{{ $movie->original_title }}
                                 </div>
                                 {{-- vote --}}
-                                <div class="vote"><strong>Voto:</strong>{{ $movie->vote }}
+                                <div class="vote mb-2 text-white"><strong>Voto: </strong>{{ $movie->vote }}
                                 </div>
                                 {{-- nationality --}}
-                                <div class="nationality"><strong>Nazionalità:</strong>{{ $movie->nationality }}
+                                <div class="nationality mb-2 text-white"><strong>Nazionalità:
+                                    </strong>{{ $movie->nationality }}
                                 </div>
                                 {{-- date --}}
-                                <div class="date"><strong>Data:</strong>{{ $movie->date }}
+                                <div class="date mb-2 text-white"><strong>Data: </strong>{{ $movie->date }}
                                 </div>
                             </div>
                         </div>
